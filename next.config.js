49b4fccb4c75ef7c0cf25531078798ff522335e0
RussoZhang <869684589@qq.com>
2025-10-25 22:36:07 -0400

    fix: clsx and Next.js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
+    // Next.js 14 默认启用 App Router，不需要 experimental.appDir
 }
 
 module.exports = nextConfig
